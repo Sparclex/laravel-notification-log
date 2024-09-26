2d6d0b221feb5af8b483b7d5b8b54357f0312284
Oliver Kaufmann <oliver.kaufmann@sanio.ch>
2024-09-26 15:59:41 +0200

    chore: code style

--- tests/Support/DummyNotificationUniqueResendable.php
+++ tests/Support/DummyNotificationUniqueResendable.php
@@ -5,10 +5,10 @@ namespace Okaufmann\LaravelNotificationLog\Tests\Support;
 use Closure;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Arr;
-use Okaufmann\LaravelNotificationLog\Concerns\HandlesResending;
 use Okaufmann\LaravelNotificationLog\Contracts\EnsureUniqueNotification;
 use Okaufmann\LaravelNotificationLog\Contracts\ResendableNotification;
 use Okaufmann\LaravelNotificationLog\Contracts\ShouldLogNotification;
+use Okaufmann\LaravelNotificationLog\Models\Concerns\HandlesResending;
 use Okaufmann\LaravelNotificationLog\Models\Concerns\HasHistory;
 use Okaufmann\LaravelNotificationLog\Models\Concerns\LogsNotifications;
 
